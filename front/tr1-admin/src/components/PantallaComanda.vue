bcfe086b354391e2a0d8b8608707b9bf84177b8d
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2024-10-23 09:54:24 +0200

    crear la pantalla comanda

---
+++ front/tr1-admin/src/components/PantallaComanda.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="8" offset-md="2">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Comandas</span>
+            </v-card-title>
+  
+            <v-card-text>
+              <v-list>
+                <v-list-item v-for="comanda in comandas" :key="comanda.id">
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Comanda #{{ comanda.id }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      Productes:
+                    </v-list-item-subtitle>
+                    <v-list-item-group>
+                      <v-list-item v-for="producto in comanda.productes" :key="producto.idProducte">
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            Producte: {{ producto.idProducte }} - Quantitat: {{ producto.quantitat }}
+                          </v-list-item-title>
+                          <v-list-item-subtitle>
+                            Preu: {{ producto.preu.toFixed(2) }} €
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  import CommunicationManager from "@/services/comManager"; 
+  
+  export default {
+    data() {
+      return {
+        comandas: [] 
+      };
+    },
+    methods: {
+      async fetchComandas() {
+        try {
+          const data = await CommunicationManager.getComandas(); 
+          this.comandas = data;
+        } catch (error) {
+          console.error("Error al obtener las comandas:", error);
+        }
+      }
+    },
+    mounted() {
+      this.fetchComandas();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
---
+++ front/tr1-admin/src/services/comManager.js
@@ -0,0 +1,19 @@
+// src/services/CommunicationManager.js
+
+const URL = 'comandas.json'; 
+
+export default {
+  async getComandas() {
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error en Communication Manager:', error);
+      throw error;
+    }
+  }
+};
