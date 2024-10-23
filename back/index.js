b456d64936f613fc446738123db2af5bc71a9c7b
Adrian Vazquez <a21adrvazvaz@inspedralbes.cat>
2024-10-23 10:18:02 +0200

    .env creado e instalado

--- back/index.js
+++ back/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
 
 app.use(bodyParser.json());
 
-
 const dbConfig = {
-    host: 'localhost', 
-    port: 3306,        
-    user: 'root',
-    password: '',
-    database: 'tr1_wired'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 async function connectToDatabase() {
