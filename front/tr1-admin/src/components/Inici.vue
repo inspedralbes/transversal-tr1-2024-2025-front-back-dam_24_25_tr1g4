758316f1fe9e7d9e3b21bace4c7f0c00efa71551
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2024-11-08 00:54:03 +0100

    afegint pantalla d'inici que al donar click als botons,redirigeixi als components (productes,comandes)

---
+++ front/tr1-admin/src/components/Inici.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="auto">
+          <v-btn density="default">Productes</v-btn>
+        </v-col>
+  
+        <v-col cols="auto">
+          <v-btn density="default">Comandes</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+   
+  </script>
+  
+  <style>
+    .v-btn{
+        align-items:center;
+
+    }
+  </style>
+  
