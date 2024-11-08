fbfc420bfb0c02d4747ddb01b4da9ac32960de3c
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2024-11-08 08:58:59 +0100

    modificacions pàgina d'inici d'administració

--- front/tr1-admin/src/components/Inici.vue
+++ front/tr1-admin/src/components/Inici.vue
@@ -1,25 +1,56 @@
 <template>
-    <v-container>
-      <v-row align="center" justify="center">
+    <v-container class="container">
+      <v-row justify="center">
         <v-col cols="auto">
-          <v-btn density="default">Productes</v-btn>
+          <h1>Administració Wired</h1>
+        </v-col>
+      </v-row>
+      <img src="../assets/logo.jpg" alt="Logo Wired">
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-btn @click="navegarAProductes" density="default">Productes</v-btn>
         </v-col>
   
         <v-col cols="auto">
-          <v-btn density="default">Comandes</v-btn>
+          <v-btn @click="navegarAComandes" density="default">Comandes</v-btn>
         </v-col>
       </v-row>
     </v-container>
   </template>
   
   <script setup>
-   
+   import { useRouter } from 'vue-router';
+
+   const router = useRouter(); 
+
+   const navegarAProductes = () => {
+    router.push('/gestioproductes');  
+  };
+  const navegarAComandes = () => {
+    router.push('/gestiocomandes'); 
+  };
   </script>
   
   <style>
-    .v-btn{
-        align-items:center;
+    .container {
+      height: 100%;
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+    }
 
+    .container>* {
+      display: flex;
+
+      align-items: center;
+      justify-content: center;
+    }
+
+    .container > img{
+      height: 500px;
+      width: 500px;
+      margin-left: auto;
+      margin-right: auto;
     }
   </style>
   
