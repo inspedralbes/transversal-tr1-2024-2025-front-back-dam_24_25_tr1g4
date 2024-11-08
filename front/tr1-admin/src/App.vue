10b7a85248fbad0a13d3ab82130cd304a0ec7009
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2024-11-08 13:22:28 +0100

    estils acabats i millorats de la pàgina

--- front/tr1-admin/src/App.vue
+++ front/tr1-admin/src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
+    <v-main >
+      <router-view class="background"/>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
+  
 </script>
+<style scoped>
+.background {
+  background: linear-gradient(210deg, #86a8e7, #c2f7f4,#cfd5e1,#6c788e);
+  background-size: 1000% 1000%;
+  animation: gradient 10s ease infinite;
+}
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
--- front/tr1-admin/src/components/Inici.vue
+++ front/tr1-admin/src/components/Inici.vue
@@ -1,56 +1,88 @@
 <template>
-    <v-container class="container">
-      <v-row justify="center">
-        <v-col cols="auto">
-          <h1>Administració Wired</h1>
-        </v-col>
-      </v-row>
-      <img src="../assets/logo.jpg" alt="Logo Wired">
-      <v-row justify="center">
-        <v-col cols="auto">
-          <v-btn @click="navegarAProductes" density="default">Productes</v-btn>
-        </v-col>
-  
-        <v-col cols="auto">
-          <v-btn @click="navegarAComandes" density="default">Comandes</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script setup>
-   import { useRouter } from 'vue-router';
+  <v-container class="container">
+    <v-row justify="center">
+      <v-col cols="auto">
+        <h1>Administració Wired</h1>
+      </v-col>
+    </v-row>
+    <img src="../assets/logo.jpg" alt="Logo Wired" />
+    <v-row justify="center">
+      <v-col cols="auto">
+        <v-btn
+          @click="navegarAProductes"
+          density="default"
+          elevation="8"
+          rounded="xl"
+          size="x-large"
+          color="normal"
+        >
+          Productes
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          @click="navegarAComandes"
+          density="default"
+          elevation="8"
+          rounded="xl"
+          size="x-large"
+          color="normal"
+        >
+          Comandes
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-   const router = useRouter(); 
+<script setup>
+import { useRouter } from 'vue-router';
 
-   const navegarAProductes = () => {
-    router.push('/gestioproductes');  
-  };
-  const navegarAComandes = () => {
-    router.push('/gestiocomandes'); 
-  };
-  </script>
-  
-  <style>
-    .container {
-      height: 100%;
-      display: flex;
-      justify-content: space-around;
-      flex-direction: column;
-    }
+const router = useRouter();
 
-    .container>* {
-      display: flex;
+const navegarAProductes = () => {
+  router.push('/gestioproductes');
+};
+const navegarAComandes = () => {
+  router.push('/gestiocomandes');
+};
+</script>
 
-      align-items: center;
-      justify-content: center;
-    }
+<style scoped>
+.container {
+  height: 100vh;
+  /* width: 100vw; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 
-    .container > img{
-      height: 500px;
-      width: 500px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-  </style>
-  
+  /* background: linear-gradient(210deg, #86a8e7, #c2f7f4,#cfd5e1,#6c788e);
+  background-size: 1000% 1000%;
+  animation: gradient 10s ease infinite; */
+}
+
+.container > * {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container > img {
+  height: 500px;
+  width: 500px;
+  margin: 0 auto;
+}
+
+/* @keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+} */
+</style>
